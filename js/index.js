759a276a40a9de112b6b8a122a124688e8e3a847
Andrewhew123 <andrew2645401@gmail.com>
2021-10-16 08:49:10 +0800

    upload new project

---
+++ js/index.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+
+
+	/*-------------------- Display item when scrolling down --------------------*/
+
+	$(window).on("scroll", function() {
+	    if($(window).scrollTop() > 350) {
+	        $(".card-project").addClass("active");
+	    } 
+	});
+
+
+	$(window).on("scroll", function() {
+	    if($(window).scrollTop() > 350) {
+	        $(".col-portfolio-image").addClass("active");
+	    } 
+	});
+
+
+	/*-------------------- Click to display --------------------*/
+	jQuery.fx.off = true;
+
+	$("#btn").on('click', function(){
+		$(".mobile-nav").toggle("#display-mobile-nav");
+		$(".overlay").toggle("#display-overlay");
+
+	});
+
+	$(".mobile-nav .mobile-nav-item").on('click', function(){
+		$(".mobile-nav").hide("#nodisplay-mobile-nav");
+		$(".overlay").hide("#nodisplay-overlay");
+
+	});
+
+
+	$(".overlay").on('click', function(){
+		$(".mobile-nav").hide("#nodisplay-mobile-nav");
+		$(".overlay").hide("#nodisplay-overlay");
+
+	});
+
+
+});
+
+
+
